89e3fabd2716eec69b35a251ec096351e26b6eae
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-05-27 08:47:33 +0200

    create html file + head and JS file + Pseudocode

---
+++ rps.js
@@ -0,0 +1,26 @@
+/*
+create variable for computer choice;
+set 3 possible choices for the computer: Rock, Paper, Scissors;
+give player (User) 3 options to choose as well: Rock, Paper, Scissors;
+check for equality;
+If equal => Tie
+If not equal check values:
+    If Computer chose rock and player scissors:
+        => "You lost! Rock beats Scissors."
+
+    If Computer chose paper and player rock:
+        => "You lost! Paper beats Rock."
+
+    If Computer chose scissors and player paper:
+        => "You lost! Scissors beat Paper."
+
+
+    If Computer chose rock and player paper:
+        => "You won! Paper beats Rock."
+
+    If Computer chose Paper and player scissors:
+        => "You won! Scissors beat Paper."
+
+    If Computer chose Scissors and player rock:
+        => "You won! Rock beats Scissors."
+*/
