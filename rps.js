b61e405f1758ef399e17a4b0c30d816cbd91b417
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-05-29 21:36:37 +0200

    Fix code structure

--- rps.js
+++ rps.js
@@ -1,19 +1,15 @@
 
 /* set 3 possible choices for computer: Rock, Paper, Scissors; */
-computerChoicePool = [
-    "Rock", "Paper", "Scissors"
-    ]
+computerChoicePool = ["Rock", "Paper", "Scissors"] 
 
-
-/* Play one round of R-P-S */
     /* Ask User for Input (Rock / Paper / Scissors */
-    /* Store User Choice in a Var */
+let PlayerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors' ");
+
     /* Run getComputerChoice and compare it to UserChoice */
     /* Declare winner in a return statement according to outcome of comparison*/
-let PlayerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors' ");
 let ComputerSelection = (getComputerChoice());
 
-function check () {
+function playRound () {
     if (PlayerSelection === "Rock" && ComputerSelection === "Scissors" 
     || PlayerSelection === "Paper" && ComputerSelection === "Rock" 
     || PlayerSelection === "Scissors" && ComputerSelection === "Paper") {
@@ -22,36 +18,26 @@ function check () {
     || PlayerSelection === "Paper" && ComputerSelection === "Scissors"
     || PlayerSelection === "Scissors" && ComputerSelection === "Rock") {
         return Lost();
-    }
-
-    else if (PlayerSelection === ComputerSelection) {
+    } else if (PlayerSelection === ComputerSelection) {
         return Tie();
     }
 }
 
-/* Create a function for Won and Lost and a function for Tie */
-
+    /* Create a function for Won and Lost and a function for Tie */
 function Won() {
     return ("You Won! " + PlayerSelection +  " beats " + ComputerSelection);
 }
-
 function Lost() {
     return ("You Lost! " + ComputerSelection + " beats " + PlayerSelection);
 }
-
 function Tie() {
     return ("It's a Tie! " + PlayerSelection + " and " + ComputerSelection + " are equal.");
 }
+
+console.log(playRound(PlayerSelection, ComputerSelection));
 
 /* create variable for computer choice; */
 function getComputerChoice() {
     var ComputerChoice = Math.floor(Math.random() * computerChoicePool.length);
     return computerChoicePool[ComputerChoice];
     }
-console.log(getComputerChoice())
-
-/* give player (User) 3 options to choose as well: Rock, Paper, Scissors;
-check for equality;
-If equal => Tie
-If not equal check values:
-    
