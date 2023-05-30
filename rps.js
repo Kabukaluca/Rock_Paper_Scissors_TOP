147e835a934ee45677789db31c66060c0e013797
Kabukaluca <130925727+Kabukaluca@users.noreply.github.com>
2023-05-30 12:33:01 +0200

    make players input case-insensitive

--- rps.js
+++ rps.js
@@ -1,19 +1,38 @@
 
-/* set 3 possible choices for computer: Rock, Paper, Scissors; */
+//set 3 possible choices for computer: Rock, Paper, Scissors;
 computerChoicePool = ["Rock", "Paper", "Scissors"] 
 
-    /* Ask User for Input (Rock / Paper / Scissors */
-let PlayerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors' ");
 
-    /* Run getComputerChoice and compare it to UserChoice */
-    /* Declare winner in a return statement according to outcome of comparison*/
+//Add 'Game - Function'
+function game() {
+    //create gameCounter
+    //loop playRound 5x
+    //after each 'playRound' increment 'gameCounter' of Winner by +1
+    //after 5 rounds end 'playRound' -loop
+    //declare the Winner (e.g. "It's 3:2, You won!")
+
+}
+
+
+/* function getPlayerChoice(){
+    var playerChoice = prompt ("Choose 'Rock', 'Paper' or 'Scissors' ")
+} */
+
+
+//Ask User for Input (Rock / Paper / Scissors
+//Make Input case Insensitive 
+let playerChoice = prompt("Choose 'Rock', 'Paper' or 'Scissors' ");
+let PlayerSelection = playerChoice.charAt(0).toUpperCase() + playerChoice.slice(1).toLowerCase();
+
+//Run getComputerChoice and compare it to UserChoice 
+//Declare winner in a return statement according to outcome of comparison
 let ComputerSelection = (getComputerChoice());
 
 function playRound () {
     if (PlayerSelection === "Rock" && ComputerSelection === "Scissors" 
     || PlayerSelection === "Paper" && ComputerSelection === "Rock" 
     || PlayerSelection === "Scissors" && ComputerSelection === "Paper") {
-        return Win();
+        return Won();
     } else if (PlayerSelection === "Rock" && ComputerSelection === "Paper"
     || PlayerSelection === "Paper" && ComputerSelection === "Scissors"
     || PlayerSelection === "Scissors" && ComputerSelection === "Rock") {
@@ -23,7 +42,7 @@ function playRound () {
     }
 }
 
-    /* Create a function for Won and Lost and a function for Tie */
+//Create a function for Won and Lost and a function for Tie
 function Won() {
     return ("You Won! " + PlayerSelection +  " beats " + ComputerSelection);
 }
@@ -36,7 +55,7 @@ function Tie() {
 
 console.log(playRound(PlayerSelection, ComputerSelection));
 
-/* create variable for computer choice; */
+//create variable for computer choice
 function getComputerChoice() {
     var ComputerChoice = Math.floor(Math.random() * computerChoicePool.length);
     return computerChoicePool[ComputerChoice];
